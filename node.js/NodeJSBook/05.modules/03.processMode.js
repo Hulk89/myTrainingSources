659fee8d9fbf9a0e4b353e0eb8eb5f945191ae78
Hyeongseok.oh <hyeongseokoh@kbjung.altibase.local.3.168.192.in-addr.arpa>
2015-11-06 16:54:42 +0900

    modules

---
+++ node.js/NodeJSBook/05.modules/03.processMode.js
@@ -0,0 +1,5 @@
+console.log( "process.execPath : " + process.execPath );
+console.log( "process.cwd() : " + process.cwd() );
+console.log( "process.version : " + process.version );
+console.log( "process.memoryUsage() : " + process.memoryUsage() );
+console.log( "process.env : " + process.env );
---
+++ node.js/NodeJSBook/05.modules/04.exportsTest.js
@@ -0,0 +1,3 @@
+var cal = require('./04.exportsEx');
+
+console.log( cal.sum(5,6) );
