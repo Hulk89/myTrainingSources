acc1edd99ab2bca98440b03bc75cb9c16948aeb5
Hyeongseok.oh <hyeongseokoh@kbjung.altibase.local.3.168.192.in-addr.arpa>
2015-11-18 13:22:46 +0900

    fix 01ex

--- node.js/NodeJSBook/07.ExpressJS/01.HeaderServer.js
+++ node.js/NodeJSBook/07.ExpressJS/01.HeaderServer.js
@@ -2,14 +2,13 @@ var http = require( 'http' );
 
 /* request의 정보를 body에 써서 내보내는 서버.
  */
-http.createServer( function( req, res )
-    {
-        var answer = "";
-        answer += "Request URL     : " + req.url + "\n";
-        answer += "Request type    : " + req.method + "\n";
-        answer += "Request headers : " + JSON.stringify( req.headers ) + "\n";
+http.createServer( function( req, res ) {
+    var answer = "";
+    answer += "Request URL     : " + req.url + "\n";
+    answer += "Request type    : " + req.method + "\n";
+    answer += "Request headers : " + JSON.stringify( req.headers ) + "\n";
 
-        res.writeHead( 200, { 'Content-Type' : 'text/plain' } );
-        res.end( 'Body\n\n' + answer );
-    }).listen( 4000, '127.0.0.1' );
+    res.writeHead( 200, { 'Content-Type' : 'text/plain' } );
+    res.end( 'Body\n\n' + answer );
+}).listen( 4000, '127.0.0.1' );
 
