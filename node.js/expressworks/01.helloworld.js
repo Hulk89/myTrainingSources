891a6cb76d497e29741f638eddae64eae2fa31d4
Hyeongseok.oh <hyeongseokoh@kbjung.altibase.local.3.168.192.in-addr.arpa>
2015-12-04 13:29:58 +0900

    remove unused files

--- node.js/expressworks/01.helloworld.js
+++
@@ -1,8 +0,0 @@
-var express = require('express')
-var app = express()
-
-app.get( '/home', function(req,res) {
-    res.writeHead( 200, { 'Content-Type': 'text/html' } );
-    res.write( 'Hello World!' );
-    res.end();
-}).listen(process.argv[2]);
