065546478aa2ff6a683a4bd86c4eabfec26f9827
Hulk89 <mitboy@naver.com>
2016-03-14 00:49:08 +0900

    finish except comments

--- DeepLearning/01.NN/NearestNeighbor.py
+++ DeepLearning/01.NN/NearestNeighbor.py
@@ -2,6 +2,8 @@
 import numpy as np
 from dataLoader import load_CIFAR
 
+testNum = 100 # Max : 10000
+
 class NearestNeighbor(object):
   def __init__(self):
     pass
@@ -11,12 +13,14 @@ class NearestNeighbor(object):
     X : N x D. 각 Row가 example이다. 
     Y : N x 1. 각 Row가 label이다.
     """
-    # the nearest neighbor classifier simply remembers all the training data
+    # Nearest Neighbor는 단순히 training data를 모두 기억한다.
     self.Xtr = X
     self.ytr = y
 
   def predict(self, X):
-    """ X is N x D where each row is an example we wish to predict label for """
+    """ 
+    X : N x D. 각 Row가 testImage이다.
+    """
     num_test = X.shape[0]
     # lets make sure that the output type matches the input type
     Ypred = np.zeros(num_test, dtype = self.ytr.dtype)
@@ -25,7 +29,8 @@ class NearestNeighbor(object):
     for i in xrange(num_test):
       # find the nearest training image to the i'th test image
       # using the L1 distance (sum of absolute value differences)
-      distances = np.sum(np.abs(self.Xtr - X[i,:]), axis = 1)
+      # distances = np.sum(np.abs(self.Xtr - X[i,:]), axis = 1)
+      distances = np.sqrt(np.sum(np.square(self.Xtr - X[i,:]), axis = 1))
       min_index = np.argmin(distances) # get the index with smallest distance
       Ypred[i] = self.ytr[min_index] # predict the label of the nearest example
 
@@ -41,8 +46,8 @@ nn = NearestNeighbor() # create a Nearest Neighbor classifier class
 
 nn.train(Xtr_rows, Ytr) # train the classifier on the training images and labels
 
-Yte_predict = nn.predict(Xte_rows) # predict labels on the test images
+Yte_predict = nn.predict(Xte_rows[0:testNum]) # predict labels on the test images
 # and now print the classification accuracy, which is the average number
 # of examples that are correctly predicted (i.e. label matches)
 
-print 'accuracy: %f' % ( np.mean(Yte_predict == Yte) )
+print 'accuracy: %f' % ( np.mean(Yte_predict == Yte[0:testNum]) )
